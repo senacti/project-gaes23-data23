c9a6a8a143a8b306a2872cb27fe83af2afa64b63
Alison Valoyes <126203688+Alison433@users.noreply.github.com>
2023-05-12 16:16:41 -0500

    php Ejercicio 1

---
+++ Exer_1.php
@@ -0,0 +1,9 @@
+//Escriba un programa para mostrar los primeros 10 números naturales.
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+    if ($i < 10) {
+        echo ', ';
+    }
+      }
+?>
