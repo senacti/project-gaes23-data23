c06f76481ba96c4bb02b310f172981970b7a9740
David Bohorquez <davidbcaro@gmail.com>
2023-05-12 16:13:47 -0500

    Se crean archivos PHP

---
+++ 02-comentarios.php
@@ -0,0 +1,7 @@
+<?php
+// Esto es un comentario de una línea
+# Esto también es un comentario de una línea
+/* Esto es un comentario de varias líneas
+que ocupa varias líneas */
+echo "Hola mundo en PHP 8";
+?>
