23df7b48dfd804b1168cbe6a8d32f6198255a3d3
Julian Santiago Millan Rodriguez <126290894+SantiagoMR02@users.noreply.github.com>
2023-05-12 16:17:07 -0500

    Archivo php

---
+++ 04_Readline.php
@@ -0,0 +1,6 @@
+<?php
+$numero1 = readline("Ingrese el primer numero");
+$numero2 = readline("Ingrese el segundo numero");
+$suma = $numero1 + $numero2;
+echo "la suma de los dos numeros es: ", $suma;
+?>
