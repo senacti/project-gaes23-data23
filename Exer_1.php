293d32c7d268eb6c29dba2cbac7f7b38f5f63463
jdstar23 <127244478+jdstar23@users.noreply.github.com>
2023-05-12 16:18:22 -0500

    Delete Exer_1.php

--- Exer_1.php
+++
@@ -1,9 +0,0 @@
-//Escriba un programa para mostrar los primeros 10 números naturales.
-<?php
-for ($i = 1; $i <= 10; $i++) {
-    echo $i;
-    if ($i < 10) {
-        echo ', ';
-    }
-      }
-?>
