7193905becceefa88f44e3fc4b292f14672d9992
David Bohorquez <davidbcaro@gmail.com>
2023-05-29 16:16:34 -0500

    Update

---
+++ data23/resources/views/contactenos.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Formulario</title>
+</head>
+<body>
+    <h1>Formulario contáctenos</h1>
+
+    <img src="{{ asset('img/React-icon.png') }}" alt="Logo de la empresa" width=20%>
+
+    <a href="{{ url('/') }}">Inicio</a>
+
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
